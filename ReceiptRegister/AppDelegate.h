f5d2f5ee31d8fb231f253255b583d6650d78fe2c
Devanathan, Jayanth <jdevanathan3@gatech.edu>
2015-06-05 18:23:31 -0400

    Initial Commit

---
+++ ReceiptRegister/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReceiptRegister
+//
+//  Created by Jayanth Devanathan on 6/5/15.
+//  Copyright (c) 2015 Jayanth Devanathan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
